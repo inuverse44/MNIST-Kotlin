dbae6fb04025b2da6ce28439db9b8a5db1c55b9c
TatsukiKodama8 <kodama_tatsuki@retail-ai.jp>
2025-12-29 00:02:26 +0900

    add: ２乗平均誤差の計算を実装

--- app/src/main/kotlin/inuverse/example/neural/loss/MeanSquaredError.kt
+++ app/src/main/kotlin/inuverse/example/neural/loss/MeanSquaredError.kt
@@ -3,10 +3,27 @@ package inuverse.example.neural.loss
 import inuverse.example.model.Vector
 
 class MeanSquaredError: Loss {
+    /**
+     * 2乗平均誤差
+     * E = \frac{1}{2N} \sum_k (y_k - t_k)^2
+     */
     override fun forward(predicted: Vector, actual: Vector): Double {
-        TODO("Not yet implemented")
+        require(predicted.size == actual.size) {
+            "Dimension mismatch: predicted.size: ${predicted.size}, actual.size: ${actual.size}"
+        }
+        val result: Double
+        var sum = 0.0
+        for (i in 0 until predicted.size) {
+            val diff = predicted[i] - actual[i]
+            sum += diff * diff
+        }
+        result = sum / (2.0 * predicted.size)
+        return result
     }
 
+    /**
+     * d Loss/d y
+     */
     override fun backward(predicted: Vector, actual: Vector): Vector {
         TODO("Not yet implemented")
     }
