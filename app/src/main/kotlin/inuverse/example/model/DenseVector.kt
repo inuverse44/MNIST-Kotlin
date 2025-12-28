73afba916ec44f6af2afaf025c2d746188e7782d
TatsukiKodama8 <kodama_tatsuki@retail-ai.jp>
2025-12-28 16:38:13 +0900

    modify: ベクトルの演算ロジックを実装。これをしないと行列のapplyの演算を定義できない。外積は後で

--- app/src/main/kotlin/inuverse/example/model/DenseVector.kt
+++ app/src/main/kotlin/inuverse/example/model/DenseVector.kt
@@ -1,4 +1,68 @@
 package inuverse.example.model
 
-class DenseVector {
+import kotlin.math.sqrt
+
+class DenseVector(
+    override val size: Int,
+    private val data: DoubleArray
+): Vector {
+    override fun get(i: Int): Double {
+        return data[i]
+    }
+
+    /**
+     * 定数倍
+     */
+    override fun scale(scale: Double): Vector {
+        val result = DoubleArray(size)
+        for (i in 0 until size) {
+            result[i] = scale * data[i]
+        }
+        return DenseVector(size, result)
+    }
+
+    /**
+     * 内積の計算
+     */
+    override fun dot(other: Vector): Double {
+        require(size == other.size) {
+            "Dimension mismatch: vector size $size and ${other.size}"
+        }
+        val result: Double
+        var sum = 0.0
+        for (i in 0 until size) {
+            sum += data[i] * other[i]
+        }
+        result = sum
+        return result
+    }
+
+    override fun cross(other: Vector): Vector {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * ノルム
+     * norm(x) = sqrt(x.dot(x))
+     */
+    override fun norm(): Double {
+        val ownVector = DenseVector(size, data)
+        val squaredNorm = this.dot(ownVector)
+        val result = sqrt(squaredNorm)
+        return result
+    }
+
+    /**
+     * 規格化
+     */
+    override fun normalize(): Vector {
+        val norm = this.norm()
+        val result = this.scale(1.0/norm)
+        return result
+    }
+
+    override fun toString(): String {
+        return data.joinToString(", ", "[", "]")
+    }
+
 }
--- app/src/main/kotlin/inuverse/example/model/Vector.kt
+++ app/src/main/kotlin/inuverse/example/model/Vector.kt
@@ -5,6 +5,10 @@ interface Vector {
 
     operator fun get(i: Int): Double
 
+    fun scale(scale: Double): Vector
+
     fun dot(other: Vector): Double
+    fun cross(other: Vector): Vector
     fun norm(): Double
+    fun normalize(): Vector
 }
