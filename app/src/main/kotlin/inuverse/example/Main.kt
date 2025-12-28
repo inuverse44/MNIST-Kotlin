6f6d723270e476bb92e59a72deee53763e3f3866
TatsukiKodama8 <kodama_tatsuki@retail-ai.jp>
2025-12-28 17:29:31 +0900

    chore: Vectorの例コンパクトにまとめた

--- app/src/main/kotlin/inuverse/example/Main.kt
+++ app/src/main/kotlin/inuverse/example/Main.kt
@@ -3,27 +3,11 @@ package inuverse.example
 import inuverse.example.example.MnistAsciiExample
 import inuverse.example.example.PlotDataExample
 import inuverse.example.example.MatrixExample
-import inuverse.example.model.DenseVector
+import inuverse.example.example.VectorExample
 
 fun main() {
-//    PlotDataExample().examplePlotLines()
-//    MnistAsciiExample().exampleMnistAscii()
-//    MatrixExample().example()
-    val vecData1 = doubleArrayOf(1.0, 2.0, 3.0)
-    val vec1 = DenseVector(vecData1.size, vecData1)
-
-    val vecData2 = doubleArrayOf(1.0, 0.0, 2.0)
-    val vec2 = DenseVector(vecData2.size, vecData2)
-
-    val scaleResult = vec1.scale(2.0)
-    val dotResult = vec1.dot(vec2)
-    val normResult = vec1.norm()
-    val normalizeResult = vec1.normalize()
-
-    println("vec1: $vec1")
-    println("vec2: $vec2")
-    println("scaleResult: $scaleResult")
-    println("dotResult: $dotResult")
-    println("normResult: $normResult")
-    println("normalizeResult: $normalizeResult")
+    PlotDataExample().examplePlotLines()
+    MnistAsciiExample().exampleMnistAscii()
+    MatrixExample().example()
+    VectorExample().example()
 }
