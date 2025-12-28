a5eacfb13c0c1c9d4409c92315179ea2bac5a820
TatsukiKodama8 <kodama_tatsuki@retail-ai.jp>
2025-12-28 22:01:45 +0900

    chore: zipとmapの使い方を学んだ

---
+++ app/src/main/kotlin/inuverse/example/example/ZipBehaviourExample.kt
@@ -0,0 +1,30 @@
+package inuverse.example.example
+
+import kotlin.math.sqrt
+
+/**
+ * zipとmapの合わせ技挙動がわからないので試してみた例
+ */
+class ZipBehaviourExample {
+    fun run() {
+        val a = doubleArrayOf(1.0, 2.0, 3.0)
+        val b = a.map {
+            val t = sqrt(it)
+            println(t)
+            t * t * t
+        }.toDoubleArray()
+        println("a: ${a.contentToString()}")
+        println("b: ${b.contentToString()}")
+
+        val c = doubleArrayOf(1.0, 0.5, 1.0/3.0)
+        val d = a.zip(c) // 型がList<Pair<Double, Double>>になっている
+        println("d: $d") // [(1.0, 1.0), (2.0, 0.5), (3.0, 0.3333333333333333)]
+        // リスト型なので、今回の設計思想的には使わない
+
+        // クソ命名変数を使っている。実際には使うな
+        val e: DoubleArray = a.zip(c).map { (it, ti) ->
+            it * ti
+        }.toDoubleArray()
+        println("e: ${e.contentToString()}")
+    }
+}
