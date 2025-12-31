dcb31b9f0cb929d8b221aefa111f83d584c4fa84
TatsukiKodama8 <kodama_tatsuki@retail-ai.jp>
2025-12-31 18:04:19 +0900

    add: Cloud Runに公開するために、Imageを作れるようにshadowを追加。Fat JARが必要

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.shadow)
 
     // Apply the application plugin to add support for building a CLI application in Java.
     application
